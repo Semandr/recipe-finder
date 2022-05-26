4015b850d270831c527a14856db2f95840bba38c
Semandr <semandr.code@gmail.com>
2022-05-26 11:05:06 +0200

    Implementing Bookmarks - part 1

--- src/js/model.js
+++ src/js/model.js
@@ -9,6 +9,7 @@ export const state = {
     page: 1,
     resultsPerPage: RES_PER_PAGE,
   },
+  bookmarks: [],
 };
 
 export const loadRecipe = async function (id) {
@@ -27,6 +28,10 @@ export const loadRecipe = async function (id) {
       ingredients: recipe.ingredients,
     };
     // console.log(state.recipe);
+
+    if (state.bookmarks.some(bookmark => bookmark.id === id))
+      state.recipe.bookmarked = true;
+    else state.recipe.bookmarked = false;
   } catch (error) {
     // Temp error handling
     console.log(`${error} 🤬 🤬 🤬`);
@@ -68,4 +73,21 @@ export const updateServings = function (newServings) {
     ing.quantity = (ing.quantity * newServings) / state.recipe.servings;
   });
   state.recipe.servings = newServings;
+};
+
+// Bookmarks
+export const addBookmark = function (recipe) {
+  // add bookmark
+  state.bookmarks.push(recipe);
+  // Mark current recipe as bookmark
+  if (recipe.id === state.recipe.id) state.recipe.bookmarked = true;
+};
+
+export const deleteBookmark = function (id) {
+  // delete bookmark
+  const index = state.bookmarks.findIndex(el => el.id === id);
+  state.bookmarks.splice(index, 1);
+
+  // Mark current recipe as NOT bookmarked anymore
+  if (id === state.recipe.id) state.recipe.bookmarked = false;
 };
