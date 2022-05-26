bd23fbf6d6b858e7b0b43fe419dd20b57f20d3af
Semandr <semandr.code@gmail.com>
2022-05-26 14:11:05 +0200

    Uploading a new Recipe - part 1

--- src/js/controller.js
+++ src/js/controller.js
@@ -91,6 +91,12 @@ const controlBookmarks = function () {
   bookmarksView.render(model.state.bookmarks);
 };
 
+const controlAddRecipe = function (newRecipe) {
+  console.log(newRecipe);
+
+  // Upload a new recipe data
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -98,5 +104,6 @@ const init = function () {
   recipeView.addHandlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
+  addRecipeView.addHandlerUpload(controlAddRecipe);
 };
 init();
--- src/js/views/addRecipeView.js
+++ src/js/views/addRecipeView.js
@@ -28,6 +28,16 @@ class AddRecipeView extends View {
     this._overlay.addEventListener('click', this.toggleWindow.bind(this));
   }
 
+  addHandlerUpload(handler) {
+    this._parentElement.addEventListener('submit', function (e) {
+      e.preventDefault();
+      // array with all the fields
+      const dataArr = [...new FormData(this)];
+      const data = Object.fromEntries(dataArr);
+      handler(data);
+    });
+  }
+
   _generateMarkup() {}
 }
 export default new AddRecipeView();
